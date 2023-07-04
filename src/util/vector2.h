d7a27aca6a00601bfefbd3e72ef36338ca666da5
anglit <ng9417265@gmail.com>
2023-07-04 17:15:37 -0400

    Added support for implicit arithmetic operators

---
+++ src/util/vector2.h
@@ -0,0 +1,50 @@
+/*
+ *  vector2.h contains the Vector2 struct and its arithmatic expressions
+ */
+
+#pragma once
+
+struct Vector2 {
+    float x;
+    float y;
+
+    Vector2 operator+(const Vector2& other) const {
+        return {x + other.x, y + other.y};
+    }
+
+    Vector2 operator-(const Vector2& other) const {
+        return {x - other.x, y - other.y};
+    }
+
+    Vector2 operator*(float scalar) const {
+        return {x * scalar, y * scalar};
+    }
+
+    Vector2 operator/(float scalar) const {
+        return {x / scalar, y / scalar};
+    }
+
+    Vector2& operator+=(const Vector2& other) {
+        x += other.x;
+        y += other.y;
+        return *this;
+    }
+
+    Vector2& operator-=(const Vector2& other) {
+        x -= other.x;
+        y -= other.y;
+        return *this;
+    }
+
+    Vector2& operator*=(float scalar) {
+        x *= scalar;
+        y *= scalar;
+        return *this;
+    }
+
+    Vector2& operator/=(float scalar) {
+        x /= scalar;
+        y /= scalar;
+        return *this;
+    }
+};
