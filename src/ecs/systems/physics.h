49215c2d5de067471526fb989d9fad8c263f79ae
anglit <ng9417265@gmail.com>
2023-07-30 12:03:59 -0400

    rewriting

--- src/ecs/systems/physics.h
+++ src/ecs/systems/physics.h
@@ -7,9 +7,7 @@
 
 #include "../system.h"
 
-class PhysicsSystem : public System {
+class PhysicsSystem {
     public:
         void Update();
-        void AABB(Entity entity_a, Entity entity_b);
-        void ResolveCollision(const CollisionEvent& collision_event);
 };
